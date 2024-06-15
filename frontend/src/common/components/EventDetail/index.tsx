51136597140f34878c21655262e6d14c997d9beb
Daniel Seiner <51313652+dannyseiner@users.noreply.github.com>
2024-06-15 12:24:07 +0200

    s

--- frontend/src/common/components/EventDetail/index.tsx
+++ frontend/src/common/components/EventDetail/index.tsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import moment from "moment";
 import React from "react";
 
@@ -19,28 +20,50 @@ const EventDetail = ({ date, onClose, events }: EventDetailProps) => {
   );
 
   return (
-    <div className="fixed bottom-[50px] left-[15%] z-10 flex w-[70%] flex-row gap-4 rounded-md border-2 border-l-primary bg-white p-4">
-      <div className="flex w-[100px] flex-col items-center justify-center">
-        <p className="w-[95px]">{moment(date).format("DD. MM. YYYY")}</p>
-        <button className="rounded-md px-2 py-1 text-xs text-primary transition-all hover:bg-primary hover:text-white">
-          přidat akci
-        </button>
-      </div>
-      {eventsInSelectedDay.length === 0 && (
-        <div className="size-full flex-1 items-center justify-center">
-          <p className="text-xs">V tento den nejsou naplánované žádné akce</p>
+    <AnimatePresence>
+      <motion.div
+        className="fixed left-[20px] z-10 flex h-[80vh] w-[300px] flex-col gap-4 rounded-md border-l-2 border-primary bg-white p-4 shadow-lg"
+        initial={{ x: "-100%", opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: "-100%", opacity: 0 }}
+        transition={{ type: "spring", stiffness: 100 }}
+      >
+        <div className="flex w-full flex-row justify-between">
+          <div className="flex flex-col">
+            <p className="text-lg font-bold">
+              {moment(date).format("DD. MM. YYYY")}
+            </p>
+          </div>
+          <button
+            className="flex size-6 items-center justify-center rounded-md bg-error text-xs uppercase text-white"
+            onClick={onClose}
+          >
+            x
+          </button>
         </div>
-      )}
-      <div className="flex-1 flex-col">
-        {eventsInSelectedDay.length !== 0 &&
-          eventsInSelectedDay.map((event: Event) => (
-            <div key={event.id} className="bg-white">
-              <p>{moment(event.date).format("HH:mm:SS")}</p>
-              <p>{event.date.toString()}</p>
-            </div>
-          ))}
-      </div>
-    </div>
+        {eventsInSelectedDay.length === 0 && (
+          <div className="size-full flex-1 items-center justify-center">
+            <p className="text-xs">V tento den nejsou naplánované žádné akce</p>
+          </div>
+        )}
+        <div className="size-full flex-col">
+          {eventsInSelectedDay.length !== 0 &&
+            eventsInSelectedDay.map((event: Event) => (
+              <div
+                key={event.id}
+                className="flex flex-row items-center gap-4 border-b border-gray-200 bg-white py-1"
+              >
+                <p className="w-[70px]">{moment(event.date).format("HH:mm")}</p>
+                <div className="h-[14px] w-[2px] rounded-full bg-primary" />
+                <p>{event.title}</p>
+              </div>
+            ))}
+        </div>
+        <button className="rounded-md bg-white px-2 py-1 text-center text-primary hover:!bg-primary hover:!text-white">
+          přidat událost
+        </button>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 export default EventDetail;
--- frontend/src/common/components/Navbar/index.tsx
+++ frontend/src/common/components/Navbar/index.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
   };
 
   useEffect(() => {
-    setCurrentUrl(asPath);
+    if (asPath) setCurrentUrl(asPath);
   }, [asPath]);
 
   return (
