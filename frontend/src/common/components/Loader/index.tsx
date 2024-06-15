753e654940c2e4dc3084fa74c8e13a6b7400a24e
Marek Machuta <markoscze@gmail.com>
2024-06-15 12:58:39 +0200

    loader

---
+++ frontend/src/common/components/Loader/index.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/no-array-index-key */
+import Lottie from "lottie-react";
+import React, { useEffect, useState } from "react";
+
+import docOClockAnimation from "@/common/animations/input-loader-animation.json";
+
+type LoaderProps = {
+  children?: React.ReactNode;
+  isLoading?: boolean;
+  isSuccess?: boolean;
+  isError?: boolean;
+  isCustom?: boolean;
+};
+
+const Loader: React.FC<LoaderProps> = ({
+  children,
+  isLoading,
+  isSuccess,
+  isError,
+  isCustom,
+}) => {
+  const [isEnded, setIsEnded] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isLoading) setIsEnded(false);
+  }, [isLoading]);
+
+  if (isEnded) return null;
+
+  if (!isCustom && (isLoading || isSuccess || isError)) {
+    if (isLoading) {
+      return (
+        <div className="absolute left-0 top-0 z-[1000] ">
+          <div className="fixed flex h-screen w-full flex-col  items-center justify-center bg-black/50 backdrop-blur-md transition-all">
+            <Lottie
+              animationData={docOClockAnimation}
+              loop
+              className="w-1/4"
+            />
+          </div>
+        </div>
+      );
+    }
+  }
+  if (!children) {
+    return null;
+  }
+
+  return (
+    <div className="absolute left-0 top-0 z-[1000] ">
+      <div className="fixed flex h-screen w-full flex-col  items-center justify-center bg-black/50 backdrop-blur-md transition-all">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
